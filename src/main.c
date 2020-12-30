b4bf82fe041be6bcc15727f7e9c3cc130f888363
Yurii Badziuk <yuriy.badziuk@posteo.de>
2020-12-30 19:58:03 +0200

    [task] Add up working blinky example for STM32 NUCLEO F303RE

--- src/main.c
+++ src/main.c
@@ -1,15 +1,49 @@
+/* Simple LED task demo, using timed delays:
+ *
+ * The LED on PC13 is toggled in task1.
+ */
+#include "FreeRTOS.h"
+#include "task.h"
+
+#include <libopencm3/stm32/rcc.h>
+#include <libopencm3/stm32/gpio.h>
+
+extern void vApplicationStackOverflowHook(
+    xTaskHandle *pxTask,
+    signed portCHAR *pcTaskName);
+
+void vApplicationStackOverflowHook(
+    xTaskHandle *pxTask __attribute((unused)),
+    signed portCHAR *pcTaskName __attribute((unused)))
+{
+  for (;;)
+    ; // Loop forever here..
+}
+
+static void
+task1(void *args __attribute((unused)))
+{
+
+  for (;;)
+  {
+    gpio_toggle(GPIOA, GPIO5);
+    vTaskDelay(pdMS_TO_TICKS(500));
+  }
+}
+
 int main(void)
 {
-  int a = 0;
-  while (1)
-  {
-    if(a){
-      ++a;
-    } else
-    {
-      --a;
-    }
-    
-  }
+  rcc_periph_clock_enable(RCC_GPIOA);
+  gpio_mode_setup(GPIOA, GPIO_MODE_OUTPUT, GPIO_PUPD_NONE, GPIO5);
+  gpio_toggle(GPIOA,GPIO5);
+  gpio_toggle(GPIOA,GPIO5);
+
+  xTaskCreate(task1, "LED", 100, NULL, configMAX_PRIORITIES - 1, NULL);
+  vTaskStartScheduler();
+
+  for (;;)
+    ;
   return 0;
 }
+
+// End
